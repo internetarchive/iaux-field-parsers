2a707f7896df40a82c096311af0f76d7f26ad853
Laton Vermette <1619661+latonv@users.noreply.github.com>
2025-06-18 22:10:25 -0700

    Upgrade dependencies to fix test coverage

--- src/field-types/page-progression.ts
+++ src/field-types/page-progression.ts
@@ -6,7 +6,8 @@ import {
 export type PageProgression = 'rl' | 'lr';
 
 export class PageProgressionParser
-  implements FieldParserInterface<PageProgression> {
+  implements FieldParserInterface<PageProgression>
+{
   // use a shared static instance for performance instead of
   // instantiating a new instance for every use
   static shared = new PageProgressionParser();
